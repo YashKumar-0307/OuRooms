7bba488e4a71d3d179c6c87e624a74b1adbd0619
Yash Kumar <ykumar1_be19@thapar.edu>
2022-01-23 17:29:58 +0530

    Getting Ready for deployment

---
+++ client/build/precache-manifest.68989658b25c480412212982eb4d8af2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ede6fa34d9a631476e65d4bc9fae6ff8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0077b36c9583f7433dcd",
+    "url": "/static/css/main.8cfa90c8.chunk.css"
+  },
+  {
+    "revision": "5979b95afb9fc2ec6e21",
+    "url": "/static/js/2.7fe4770d.chunk.js"
+  },
+  {
+    "revision": "0077b36c9583f7433dcd",
+    "url": "/static/js/main.3a475ff8.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  },
+  {
+    "revision": "9cb05efb6597a87ed4e56a6dc1e24dfb",
+    "url": "/static/media/chatLogo.9cb05efb.png"
+  },
+  {
+    "revision": "544a6e387c708f34eb292bce21699c26",
+    "url": "/static/media/location.544a6e38.png"
+  },
+  {
+    "revision": "98cdd020d5b5866c7498b7b67eae2680",
+    "url": "/static/media/logo1.98cdd020.png"
+  }
+]);
